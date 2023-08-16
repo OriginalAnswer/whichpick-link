36cbdb82f7437aa3e98bd1bb2273de6d7fb15b9e
OriginalAnswer <akiseonair@gmail.com>
2023-08-16 21:47:37 +0900

    .

--- search.js
+++ search.js
@@ -1,15 +1,47 @@
+// const originView = document.querySelector('.links');
+// const currentView = document.querySelector('.currentview');
+// const allLinks = document.querySelectorAll('.link'); 
+// function search(v) {
+//     if (v == "") {
+//         originView.classList.remove('dpnone')
+//         currentView.classList.add('dpnone');
+//     } else {
+//         originView.classList.add('dpnone');
+//         currentView.classList.remove('dpnone')
+//     };
+//     const n = document.querySelector('.item-name');
+// }
 const originView = document.querySelector('.links');
 const currentView = document.querySelector('.currentview');
 const allLinks = document.querySelectorAll('.link'); 
-function search(v) {
-    if (v == "") {
-        originView.classList.remove('dpnone')
-        currentView.classList.add('dpnone');
-    } else {
-        originView.classList.add('dpnone');
-        currentView.classList.remove('dpnone')
-    };
-    const n = document.querySelector('.item-name');
+const inputSearch = document.getElementById('input-search');
+const f = document.querySelector('footer');
 
-    for (var i = 0; i < n.lenght)
+function search() {
+    const searchValue = inputSearch.value;
+    const linkToShow = document.getElementById(searchValue);
+    console.log(searchValue)
+    if (searchValue > 0) {
+        // 모든 링크를 숨기기
+        allLinks.forEach(link => {
+            link.classList.add('dpnone');
+        });
+        // 해당 id를 가진 링크만 보이기
+        // if (linkToShow) {
+        //     console.log(linkToShow)
+        // }
+        linkToShow.classList.remove('dpnone');
+        f.style.marginTop = '300px';
+    } else {
+        allLinks.forEach(link => {
+            link.classList.remove('dpnone');
+        });
+        
+        f.style.marginTop = '30px';
+    }
 }
+search();
+console.log(inputSearch.value);
+// inputSearch.addEventListener('keyup', function() {
+//     search(this.value);
+// });
